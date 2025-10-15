a65eb39fd6176300f304651d5ded72b01bc42a7a
AbyxDev <ken@abyx.dev>
2025-10-14 20:04:14 -0400

    Add voxel_gpu to qsys

--- firmware/address_map_arm.h
+++ firmware/address_map_arm.h
@@ -21,13 +21,10 @@
 #define HEX5_HEX4_BASE        0xFF200030
 #define SW_BASE               0xFF200040
 #define KEY_BASE              0xFF200050
-#define JP1_BASE              0xFF200060
-#define JP2_BASE              0xFF200070
 #define PS2_BASE              0xFF200100
 #define PS2_DUAL_BASE         0xFF200108
 #define JTAG_UART_BASE        0xFF201000
 #define JTAG_UART_2_BASE      0xFF201008
-#define IrDA_BASE             0xFF201020
 #define TIMER_BASE            0xFF202000
 #define TIMER_2_BASE          0xFF202020
 #define AV_CONFIG_BASE        0xFF203000
@@ -35,8 +32,6 @@
 #define PIXEL_BUF_CTRL_BASE   0xFF203020
 #define CHAR_BUF_CTRL_BASE    0xFF203030
 #define AUDIO_BASE            0xFF203040
-#define VIDEO_IN_BASE         0xFF203060
-#define EDGE_DETECT_CTRL_BASE 0xFF203070
 #define ADC_BASE              0xFF204000
 
 /* Cyclone V HPS devices */
