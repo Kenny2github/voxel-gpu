096fac999472c1b6688a276c8633b86829b46196
AbyxDev <ken@abyx.dev>
2025-10-15 17:00:24 -0400

    New default test main program

--- software/main.c
+++ software/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-
-int * const DEADBEEF = (int *)0xFF20BEF0;
+#include "address_map_arm.h"
+#include "hardware.h"
 
 int main(void) {
-	printf("Hello world!\n");
-	printf("0xFF20BEF0 = %08X\n", *DEADBEEF);
+	printf("0xFF565800: pixel_buffer1 = %08X\n", GPU->pixel_buffer1);
+	printf("0xFF565804: pixel_buffer2 = %08X\n", GPU->pixel_buffer2);
+	GPU->pixel_buffer2 = SDRAM_BASE;
+	printf("0xFF565804: pixel_buffer2 = %08X\n", GPU->pixel_buffer2);
 	while (1);
 }
