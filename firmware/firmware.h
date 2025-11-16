aca60b30896445dcd7ed998f0afbbd569d43aac7
arthursyc <arthur1chan@gmail.com>
2025-11-16 01:03:27 -0500

    small draft of voxel space init + set voxel

--- firmware/firmware.h
+++ firmware/firmware.h
@@ -5,23 +5,23 @@
 #define BLANK 0x0
 #define WHITE 0x1
 
-#define GRID_START 0xC2000000 // for now ive defined it as halfway of SDRAM, placement tbd
+#define GRID_START 0xC3000000 // for now ive defined it as the lower end of SDRAM occupying (2^16^3)B, placement tbd
 
 /* main */
 void init_firmware();
 
 /* voxels */
 typedef struct v_pos {
-    uint16_t x;
-    uint16_t y;
-    uint16_t z;
+    uint8_t x;
+    uint8_t y;
+    uint8_t z;
 } v_pos;
 
-// void init_voxel_space();
+void init_voxel_space(uint8_t len);
 
-void set_voxel(v_pos pos, uint16_t palette);
+void set_voxel(v_pos pos, uint8_t palette);
 
-void set_voxel_range(v_pos corner0, v_pos corner1, uint16_t palette);
+void set_voxel_range(v_pos corner0, v_pos corner1, uint8_t palette);
 
 /* camera */
 typedef struct cam_pos {
--- firmware/voxel.c
+++ firmware/voxel.c
@@ -1,6 +1,59 @@
 #include "hardware/hardware.h"
 #include "firmware/firmware.h"
 
-void set_voxel(v_pos pos, uint16_t palette) {
-    
+#define abs(x) ((x >= 0) ? (x) : (-x))
+
+uint8_t side_len = 8;
+
+void init_voxel_space(uint8_t len) {
+    side_len = len;
+}
+
+inline bool within_range(v_pos pos) {
+    return (pos.x < side_len && pos.y < side_len && pos.z < side_len);
+} 
+
+void set_voxel(v_pos pos, uint8_t palette) {
+    *(GRID_START + pos.x + pos.y * side_len + pos.z * side_len * side_len) = palette;
+}
+
+void set_voxel_range(v_pos corner0, v_pos corner1, uint8_t palette) {
+    assert (within_range(corner0) && within_range(corner1));
+
+    v_pos start;
+    v_pos end;
+    uint8_t dist_x;
+
+    if (corner0.x < corner1.x) {
+        start.x = corner0.x;
+        end.x = corner1.x;
+    } else {
+        start.x = corner1.x;
+        end.x = corner0.x;
+    }
+    dist_x = end.x - start.x + 1;
+
+    if (corner0.y < corner1.y) {
+        start.y = corner0.y;
+        end.y = corner1.y;
+    } else {
+        start.y = corner1.y;
+        end.y = corner0.y;
+    }
+    if (corner0.z < corner1.z) {
+        start.z = corner0.z;
+        end.z = corner1.z;
+    } else {
+        start.z = corner1.z;
+        end.z = corner0.z;
+    }
+
+    /* xy horizontal, y vertical */
+    uint8_t offset;
+    for (uint8_t y = start.y; y <= end.y; ++y) {
+        offset = GRID_START + y * side_len * side_len;
+        for (uint8_t z = start.z; z <= end.z; ++z) {
+            memset(offset + z * sidelen, palette, dist_x);
+        }
+    }
 }
