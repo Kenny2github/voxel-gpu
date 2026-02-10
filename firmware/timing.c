03e967f40c71ccc76317f47495c8de1e8c0d3bbd
arthursyc <arthur1chan@gmail.com>
2026-02-10 15:58:22 -0500

    changes from sparse-software

--- firmware/timing.c
+++ firmware/timing.c
@@ -20,23 +20,24 @@ static const uint8_t num_to_hex[10] = {
 };
 
 void enable_timer_interrupt(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
 	// timer is 200MHz
-	*(timer_ptr) = 200E6;
+	MPCORE_PRIV_TIMER->load = 200E6;
 	// enable interrupts and countdown
-	*(timer_ptr + 2) = 0b111;
+	MPCORE_PRIV_TIMER->e = 1;
+	MPCORE_PRIV_TIMER->a = 1;
+	MPCORE_PRIV_TIMER->i = 1;
 }
 
 static void handle_timer_interrupt(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
 	// reset interrupt bit by writting 1 to it
-	*(timer_ptr + 3) &= 1;
-	uint32_t frames_display = num_to_hex[frames % 10];
-	frames_display |= num_to_hex[(frames / 10) % 10] << 8;
-	frames_display |= num_to_hex[(frames / 100) % 10] << 16;
-	frames_display |= num_to_hex[(frames / 1000) % 10] << 24;
-	memset(HEX3_HEX0, frames_display, 4);
-	printf("GPU latency: %e sec\n", gpu_latency);
+	MPCORE_PRIV_TIMER->f = 1;
+	struct hex3_hex0_registers frames_display;
+	frames_display.hex0 = num_to_hex[frames % 10];
+	frames_display.hex1 = num_to_hex[(frames / 10) % 10];
+	frames_display.hex2 = num_to_hex[(frames / 100) % 10];
+	frames_display.hex3 = num_to_hex[(frames / 1000) % 10];
+	*HEX3_HEX0 = frames_display;
+	// printf("GPU latency: %e sec\n", gpu_latency);
 	frames = 0;
 	++fw_time;
 }
@@ -46,15 +47,16 @@ void enable_timer(void) {
 	fw_time = 0;
 	gpu_latency = 0;
 	config_interrupt(PRIVATE_TIMER_IRQ, NULL, &handle_timer_interrupt);
+	enable_timer_interrupt();
 }
 
 void disable_timer(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
 	// disable interrupts, auto-reload and countdown
-	*(timer_ptr + 2) = 0b000;
+	MPCORE_PRIV_TIMER->e = 0;
+	MPCORE_PRIV_TIMER->a = 0;
+	MPCORE_PRIV_TIMER->i = 0;
 }
 
 uint32_t cur_time(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
-	return *(timer_ptr + 1);
+	return MPCORE_PRIV_TIMER->counter;
 }
