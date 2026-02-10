049bdb2b63f1cd8ec01a2b70eeab70f6d1123691
AbyxDev <ken@abyx.dev>
2026-02-10 10:59:44 -0500

    Ensure full-word writes to certain registers
    Macros for packed aligned structs and ensuring word-size structs

--- hardware/hardware.h
+++ hardware/hardware.h
@@ -4,6 +4,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#define assert_word_size(type, name)                                           \
+    _Static_assert(sizeof(type) == sizeof(uint32_t), name " must be word size")
+#define PA_STRUCT struct __attribute__((__packed__, __aligned__(4)))
+
 // NOTE: In little-endian ARM, struct bitfields are in LSB-to-MSB order;
 // volatile structs make all their members volatile; and
 // volatile ints must always be accessed by their whole width.
@@ -83,55 +87,59 @@ extern volatile unsigned char *const FPGA_PIXEL_BUF_BASE;
 extern volatile unsigned char *const FPGA_CHAR_BASE;
 #define FPGA_CHAR_END 0xC9001FFF
 
-struct __attribute__((__packed__, __aligned__(4))) ledr_registers {
+PA_STRUCT ledr_registers {
     uint32_t ledr : 10;
-    uint32_t _unused : 22;
+    uint32_t : 22;
 };
 extern volatile struct ledr_registers *const LED;
 extern volatile struct ledr_registers *const LEDR;
 
-struct __attribute__((__packed__, __aligned__(4))) hex3_hex0_registers {
+PA_STRUCT hex3_hex0_registers {
     uint32_t hex0 : 8;
     uint32_t hex1 : 8;
     uint32_t hex2 : 8;
     uint32_t hex3 : 8;
 };
+assert_word_size(struct hex3_hex0_registers, "HEX3-0 register");
 extern volatile struct hex3_hex0_registers *const HEX3_HEX0;
 
-struct __attribute__((__packed__, __aligned__(4))) hex5_hex4_registers {
+PA_STRUCT hex5_hex4_registers {
     uint32_t hex4 : 8;
     uint32_t hex5 : 8;
-    uint32_t _unused : 16;
+    uint32_t : 16;
 };
+assert_word_size(struct hex5_hex4_registers, "HEX5-4 register");
 extern volatile struct hex5_hex4_registers *const HEX5_HEX4;
 
-struct __attribute__((__packed__, __aligned__(4))) sw_registers {
+PA_STRUCT sw_registers {
     uint32_t sw : 10;
-    uint32_t _unused : 22;
+    uint32_t : 22;
 };
+assert_word_size(struct sw_registers, "SW register");
 extern volatile struct sw_registers *const SW;
 
-struct __attribute__((__packed__, __aligned__(4))) key_registers {
-    uint32_t key : 4;
+PA_STRUCT key_bits_register {
+    uint32_t bits : 4;
     uint32_t : 28;
+};
+assert_word_size(struct key_bits_register, "KEY registers");
+PA_STRUCT key_registers {
+    struct key_bits_register key;
     uint32_t : 32;
-    uint32_t mask : 4;
-    uint32_t : 28;
-    uint32_t edge : 4;
-    uint32_t : 28;
+    struct key_bits_register mask;
+    struct key_bits_register edge;
 };
 extern volatile struct key_registers *const KEY;
 #define KEY_IRQ 1U
 
-struct __attribute__((__packed__, __aligned__(4))) ps2_data {
+PA_STRUCT ps2_data {
     uint32_t data : 8;
     uint32_t : 7;
     uint32_t rvalid : 1;
     uint32_t ravail : 16;
 };
-
-struct __attribute__((__packed__, __aligned__(4))) ps2_registers {
-    struct ps2_data data;
+assert_word_size(struct ps2_data, "PS2 data type");
+PA_STRUCT ps2_flags {
     uint32_t re : 1;
     uint32_t : 7;
     uint32_t ri : 1;
@@ -139,16 +147,24 @@ struct __attribute__((__packed__, __aligned__(4))) ps2_registers {
     uint32_t ce : 1;
     uint32_t : 21;
 };
+assert_word_size(struct ps2_flags, "PS2 flags type");
+PA_STRUCT ps2_registers {
+    struct ps2_data data;
+    struct ps2_flags flags;
+};
 extern volatile struct ps2_registers *const PS2;
 #define PS2_IRQ 79U
 extern volatile struct ps2_registers *const PS2_DUAL;
 #define PS2_DUAL_IRQ 89U
 
-struct __attribute__((__packed__, __aligned__(4))) jtag_uart_registers {
+PA_STRUCT jtag_uart_data {
     uint32_t data : 8;
     uint32_t : 7;
     uint32_t rvalid : 1;
     uint32_t ravail : 16;
+};
+assert_word_size(struct jtag_uart_data, "JTAG UART data type");
+PA_STRUCT jtag_uart_flags {
     uint32_t re : 1;
     uint32_t we : 1;
     uint32_t : 6;
@@ -158,19 +174,36 @@ struct __attribute__((__packed__, __aligned__(4))) jtag_uart_registers {
     uint32_t : 5;
     uint32_t wspace : 16;
 };
+assert_word_size(struct jtag_uart_flags, "JTAG UART flags type");
+PA_STRUCT jtag_uart_registers {
+    struct jtag_uart_data data;
+    struct jtag_uart_flags flags;
+};
 extern volatile struct jtag_uart_registers *const JTAG_UART;
 extern volatile struct jtag_uart_registers *const JTAG_UART_2;
 #define JTAG_UART_IRQ 8U
 
-struct __attribute__((__packed__, __aligned__(4))) timer_registers {
+PA_STRUCT timer_status_register {
     uint32_t to : 1;
     uint32_t run : 1;
     uint32_t : 30;
+};
+assert_word_size(
+    struct timer_status_register, "Interval timer status register type"
+);
+PA_STRUCT timer_control_register {
     uint32_t ito : 1;
     uint32_t cont : 1;
     uint32_t start : 1;
     uint32_t stop : 1;
     uint32_t : 28;
+};
+assert_word_size(
+    struct timer_control_register, "Interval timer control register type"
+);
+PA_STRUCT timer_registers {
+    struct timer_status_register status;
+    struct timer_control_register control;
     uint32_t start_low : 16;
     uint32_t : 16;
     uint32_t start_high : 16;
@@ -185,33 +218,43 @@ extern volatile struct timer_registers *const TIMER_2;
 #define TIMER_IRQ 0U
 #define TIMER_2_IRQ 2U
 
-struct __attribute__((__packed__, __aligned__(4))) buf_ctrl_registers {
+PA_STRUCT buf_ctrl_status_register {
+    uint32_t s : 1;
+    uint32_t a : 1;
+    uint32_t en : 1;
+    uint32_t : 3;
+    uint32_t sb : 2;
+    uint32_t bs : 4;
+    uint32_t : 4;
+    uint32_t n : 8;
+    uint32_t m : 8;
+};
+assert_word_size(
+    struct buf_ctrl_status_register, "Buffer control status register type"
+);
+PA_STRUCT buf_ctrl_registers {
     unsigned char *buffer;
     unsigned char *back_buffer;
     uint32_t x_resolution : 16;
     uint32_t y_resolution : 16;
-    struct __attribute__((__packed__, __aligned__(4))) {
-        uint32_t s : 1;
-        uint32_t a : 1;
-        uint32_t en : 1;
-        uint32_t : 3;
-        uint32_t sb : 2;
-        uint32_t bs : 4;
-        uint32_t : 4;
-        uint32_t n : 8;
-        uint32_t m : 8;
-    } status;
+    struct buf_ctrl_status_register status;
 };
 extern volatile struct buf_ctrl_registers *const PIXEL_BUF_CTRL;
 extern volatile struct buf_ctrl_registers *const CHAR_BUF_CTRL;
 
-struct __attribute__((__packed__, __aligned__(4))) hps_timer_registers {
-    uint32_t load;
-    uint32_t counter;
+PA_STRUCT hps_timer_control_register {
     uint32_t e : 1;
     uint32_t m : 1;
     uint32_t i : 1;
     uint32_t : 29;
+};
+assert_word_size(
+    struct hps_timer_control_register, "HPS timer control register type"
+);
+PA_STRUCT hps_timer_registers {
+    uint32_t load;
+    uint32_t counter;
+    struct hps_timer_control_register control;
     uint32_t f : 1;
     uint32_t : 31;
     uint32_t s : 1;
@@ -223,23 +266,30 @@ extern volatile struct hps_timer_registers *const HPS_TIMER[4];
 #define HPS_TIMER_2_IRQ 201
 #define HPS_TIMER_3_IRQ 202
 
-struct __attribute__((__packed__, __aligned__(4))) fpga_bridge_registers {
+PA_STRUCT fpga_bridge_registers {
     uint32_t hps2fpga_reset : 1;
     uint32_t lwhps2fpga_reset : 1;
     uint32_t fpga2hps_reset : 1;
     uint32_t : 29;
 };
+assert_word_size(struct fpga_bridge_registers, "FPGA bridge register type");
 extern volatile struct fpga_bridge_registers *const FPGA_BRIDGE;
 
-struct __attribute__((__packed__, __aligned__(4))) private_timer_registers {
-    uint32_t load;
-    uint32_t counter;
+PA_STRUCT private_timer_control_register {
     uint32_t e : 1;
     uint32_t a : 1;
     uint32_t i : 1;
     uint32_t : 5;
     uint32_t prescaler : 8;
     uint32_t : 16;
+};
+assert_word_size(
+    struct private_timer_control_register, "Private timer control register type"
+);
+PA_STRUCT private_timer_registers {
+    uint32_t load;
+    uint32_t counter;
+    struct private_timer_control_register control;
     uint32_t f : 1;
     uint32_t : 31;
 };
@@ -247,20 +297,20 @@ extern volatile struct private_timer_registers *const MPCORE_PRIV_TIMER;
 #define PRIVATE_TIMER_IRQ 29
 
 struct __attribute__((__packed__)) gic_cpuif_registers {
-	/* CPU interface control register */
-	uint32_t iccicr;
-	/* interrupt priority mask register */
-	uint32_t iccpmr;
+    /* CPU interface control register */
+    uint32_t iccicr;
+    /* interrupt priority mask register */
+    uint32_t iccpmr;
     /* 4-byte offset */
     uint32_t _reserved0;
-	/* interrupt acknowledge register */
-	uint32_t icciar;
-	/* end of interrupt register */
-	uint32_t icceoir;
+    /* interrupt acknowledge register */
+    uint32_t icciar;
+    /* end of interrupt register */
+    uint32_t icceoir;
 };
 extern volatile struct gic_cpuif_registers *const MPCORE_GIC_CPUIF;
 
-struct __attribute__((__packed__, __aligned__(4))) gic_dist_registers {
+PA_STRUCT gic_dist_registers {
     /* distributor control register */
     uint32_t icddcr;
     uint32_t icdictr;
@@ -340,6 +390,7 @@ union cpsr_t {
         uint32_t N : 1;
     } fields;
 };
+assert_word_size(union cpsr_t, "CPSR register type");
 #define MODE_SVC 0b10011
 #define MODE_IRQ 0b10010
 
