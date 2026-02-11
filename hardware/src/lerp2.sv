e12b1aea878443bd643ef751ee1eecd40fcf193c
AbyxDev <ken@abyx.dev>
2026-02-10 19:47:01 -0500

    lerp2: output "error" signal on divide by zero or overflow

--- hardware/src/lerp2.sv
+++ hardware/src/lerp2.sv
@@ -13,6 +13,7 @@ module lerp2 #(
     output logic signed [WIDTH-1:0] val,
     input logic start,
     output logic done,
+    output logic error,
     input logic clock,
     input logic reset
 );
@@ -20,6 +21,8 @@ module lerp2 #(
   assign val = p0 + lerp_x + lerp_y + lerp_xy;
   wand _done;
   assign done = _done;
+  wor _error;
+  assign error = _error;
 
   div #(
       .WIDTH(WIDTH),
@@ -31,8 +34,8 @@ module lerp2 #(
       .valid(_done),
       .busy(),
       .done(_done),
-      .dbz(),
-      .ovf(),
+      .dbz(_error),
+      .ovf(_error),
       .a(WIDTH'(((p1 - p0) * {{WIDTH{x[WIDTH-1]}}, x}) >> FBITS)),
       .b(X),
       .val(lerp_x)
@@ -47,8 +50,8 @@ module lerp2 #(
       .valid(_done),
       .busy(),
       .done(_done),
-      .dbz(),
-      .ovf(),
+      .dbz(_error),
+      .ovf(_error),
       .a(WIDTH'(((p2 - p0) * {{WIDTH{y[WIDTH-1]}}, y}) >> FBITS)),
       .b(Y),
       .val(lerp_y)
@@ -63,8 +66,8 @@ module lerp2 #(
       .valid(_done),
       .busy(),
       .done(_done),
-      .dbz(),
-      .ovf(),
+      .dbz(_error),
+      .ovf(_error),
       .a(WIDTH'(((p0 - p1 + p3 - p2) * {{WIDTH{x[WIDTH-1]}}, x} * y) >> (2 * FBITS))),
       .b(WIDTH'(({{WIDTH{X[WIDTH-1]}}, X} * Y) >> FBITS)),
       .val(lerp_xy)
