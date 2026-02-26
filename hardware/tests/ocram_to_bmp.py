4fae32cf36d8ba7643055ad09934662dd80dcb18
AbyxDev <ken@abyx.dev>
2026-02-26 15:40:16 -0500

    ocram_to_bmp.py: Account for X-Y addressing

--- hardware/tests/ocram_to_bmp.py
+++ hardware/tests/ocram_to_bmp.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 from PIL import Image
 
+WIDTH = 320
+HEIGHT = 240
+
 with open('ocram.hex') as f:
-    pixels = bytearray()
+    data = bytearray()
     for i, line in enumerate(f):
         if line.startswith('//'):
             continue
         word = int(line, 16)
+        data.extend(word.to_bytes(4, 'little'))
+
+pixels = bytearray()
+for row in range(HEIGHT):
+    for col in range(WIDTH):
+        coord = (row << 10) | (col << 1)
+        word = int.from_bytes(data[coord:coord+2], 'little')
         r = ((word & 0xf100) >> (6 + 5)) * 0xff // 0x1f
         g = ((word & 0x07e0) >> 5) * 0xff // 0x3f
         b = (word & 0x001f) * 0xff // 0x1f
