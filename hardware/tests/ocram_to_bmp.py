e1a005600d6fc2e5706972f44ddad87e08165699
AbyxDev <ken@abyx.dev>
2026-03-06 18:04:13 -0500

    Make ocram_to_bmp importable

--- hardware/tests/ocram_to_bmp.py
+++ hardware/tests/ocram_to_bmp.py
@@ -4,25 +4,34 @@ from PIL import Image
 WIDTH = 320
 HEIGHT = 240
 
-with open('ocram.hex') as f:
+def hex_to_bytes(filename: str) -> bytearray:
     data = bytearray()
-    for i, line in enumerate(f):
-        if line.startswith('//'):
-            continue
-        word = int(line, 16)
-        data.extend(word.to_bytes(4, 'little'))
+    with open(filename) as f:
+        for i, line in enumerate(f):
+            if line.startswith('//'):
+                continue
+            word = int(line, 16)
+            data.extend(word.to_bytes(4, 'little'))
+    return data
 
-pixels = bytearray()
-for row in range(HEIGHT):
-    for col in range(WIDTH):
-        coord = (row << 10) | (col << 1)
-        word = int.from_bytes(data[coord:coord+2], 'little')
-        r = ((word & 0xf100) >> (6 + 5)) * 0xff // 0x1f
-        g = ((word & 0x07e0) >> 5) * 0xff // 0x3f
-        b = (word & 0x001f) * 0xff // 0x1f
-        pixels.extend((r, g, b))
-        r = ((word & 0xf1000000) >> (6 + 5 + 16)) * 0xff // 0x1f
-        g = ((word & 0x07e00000) >> (5 + 16)) * 0xff // 0x3f
-        b = ((word & 0x001f0000) >> 16) * 0xff // 0x1f
-        pixels.extend((r, g, b))
-Image.frombytes('RGB', (256, 192), pixels).save('ocram.bmp')
+def vga_to_rgb(data: bytearray) -> bytearray:
+    pixels = bytearray()
+    for row in range(HEIGHT):
+        for col in range(WIDTH):
+            coord = (row << 10) | (col << 1)
+            word = int.from_bytes(data[coord:coord+2], 'little')
+            r = ((word & 0xf100) >> (6 + 5)) * 0xff // 0x1f
+            g = ((word & 0x07e0) >> 5) * 0xff // 0x3f
+            b = (word & 0x001f) * 0xff // 0x1f
+            pixels.extend((r, g, b))
+            r = ((word & 0xf1000000) >> (6 + 5 + 16)) * 0xff // 0x1f
+            g = ((word & 0x07e00000) >> (5 + 16)) * 0xff // 0x3f
+            b = ((word & 0x001f0000) >> 16) * 0xff // 0x1f
+            pixels.extend((r, g, b))
+    return pixels
+
+if __name__ == '__main__':
+    Image.frombytes(
+        'RGB', (WIDTH, HEIGHT),
+        vga_to_rgb(hex_to_bytes('ocram.hex'))
+    ).save('ocram.bmp')
