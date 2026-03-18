3212976fa0b5c4a372e8a72725c46f06e488fb7d
arthursyc <arthur1chan@gmail.com>
2026-03-17 20:18:28 -0400

    switch out of simulation in SW, with test voxel scene

--- software/main.c
+++ software/main.c
@@ -29,28 +29,36 @@ int main(void) {
     set_camera_settings_software(90.0, 1);
 
     // Setting up camera
-    struct Vector camPos = {34, 34, 36};
-    struct Vector camLook = {0, 0, -1};
+    struct Vector camPos = {5, 0.5, 0.5};
+    struct Vector camLook = {-1, 0, 0};
     struct Vector camUp = {0, 1, 0};
     set_camera_default(camPos, camLook, camUp);
     set_camera_default_software(camPos, camLook, camUp);
 
     // Setting up voxels
-    v_pos startPos = {32, 32, 32};
-    v_pos firstPos = {0, 0, 0};
-    v_pos endPos = {SIDE_LEN-1, SIDE_LEN-1, SIDE_LEN-1};
+    // v_pos startPos = {32, 32, 32};
+    // v_pos firstPos = {0, 0, 0};
+    // v_pos endPos = {SIDE_LEN-1, SIDE_LEN-1, SIDE_LEN-1};
     // fill_voxel_range(firstPos, endPos, 0x0);
-    
-    set_voxel(startPos, 1);
+
+    set_voxel((v_pos){1,0,0}, 1);
+    set_voxel((v_pos){1,2,2}, 1);
+    set_voxel((v_pos){1,2,-2}, 1);
+    set_voxel((v_pos){1,-2,2}, 1);
+    set_voxel((v_pos){1,-2,-2}, 1);
+    set_voxel((v_pos){-1,2,2}, 1);
+    set_voxel((v_pos){-1,2,-2}, 1);
+    set_voxel((v_pos){-1,-2,2}, 1);
+    set_voxel((v_pos){-1,-2,-2}, 1);
     // set_voxel((v_pos){34, 32, 32}, 1);
     // load_monkey();
-    clear_screen_software();
-    wait_for_vsync_software(); // wait_for_vsync();
+    // clear_screen_software();
+    // wait_for_vsync_software(); // wait_for_vsync();
 
     uint8_t handle_enter_key = 0;
 
     while(1) {
-        
+
         if (enter_key_held)
         {
             if (!handle_enter_key)
@@ -61,7 +69,7 @@ int main(void) {
                 {
                     v_pos new_voxel_pos = {target_x, target_y, target_z};
                     set_voxel(new_voxel_pos, 1);
-                    printf("Voxel placed at: %d, %d, %d\n", target_x, target_y, target_z); // Comment out later
+                    // printf("Voxel placed at: %d, %d, %d\n", target_x, target_y, target_z); // Comment out later
                 }
 
                 handle_enter_key = 1;
@@ -72,8 +80,7 @@ int main(void) {
             handle_enter_key = 0;
         }
 
-        clear_screen_software();
-        render_software(); // render();
-        wait_for_vsync_software(); // wait_for_vsync();
+        // clear_screen_software();
+        render();
     }
 }
