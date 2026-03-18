b8e64ee9ed6bb975eb50295195f93aa431dec8b2
AbyxDev <ken@abyx.dev>
2026-03-17 23:11:05 -0400

    timing.c: use hardware.h types

--- firmware/timing.c
+++ firmware/timing.c
@@ -40,22 +40,24 @@ static void write_gpu_latency() {
 }
 
 void enable_timer_interrupt(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
-	// timer is 200MHz
-	*(timer_ptr) = 200E6;
+	MPCORE_PRIV_TIMER->load = 200E6;
 	// enable interrupts and countdown
-	*(timer_ptr + 2) = 0b111;
+	MPCORE_PRIV_TIMER->control = (struct private_timer_control_register){
+		.i = 1,
+		.a = 1,
+		.e = 1
+	};
 }
 
 static void handle_timer_interrupt(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
 	// reset interrupt bit by writting 1 to it
-	*(timer_ptr + 3) &= 1;
-	uint32_t frames_display = num_to_hex[frames % 10];
-	frames_display |= num_to_hex[(frames / 10) % 10] << 8;
-	frames_display |= num_to_hex[(frames / 100) % 10] << 16;
-	frames_display |= num_to_hex[(frames / 1000) % 10] << 24;
-	memset(HEX3_HEX0, frames_display, 4);
+	MPCORE_PRIV_TIMER->f = 1;
+	struct hex3_hex0_registers frames_display;
+	frames_display.hex0 = num_to_hex[frames % 10];
+	frames_display.hex1 = num_to_hex[(frames / 10) % 10];
+	frames_display.hex2 = num_to_hex[(frames / 100) % 10];
+	frames_display.hex3 = num_to_hex[(frames / 1000) % 10];
+	*HEX3_HEX0 = frames_display;
 	write_gpu_latency();
 	frames = 0;
 	++fw_time;
@@ -69,12 +71,14 @@ void enable_timer(void) {
 }
 
 void disable_timer(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
 	// disable interrupts, auto-reload and countdown
-	*(timer_ptr + 2) = 0b000;
+	MPCORE_PRIV_TIMER->control = (struct private_timer_control_register){
+		.i = 0,
+		.a = 0,
+		.e = 0
+	};
 }
 
 uint32_t cur_time(void) {
-	volatile int* timer_ptr = (int*)MPCORE_PRIV_TIMER;
-	return *(timer_ptr + 1);
+	return MPCORE_PRIV_TIMER->counter;
 }
